af253255e29dd07cd88462e399689d63dd9506c1
TheTechWolfLR <lorenzoar43@gmail.com>
2025-03-26 15:47:16 +0100

    turtel fix en prefabs

--- Imagine-game/Assets/scripts/TurtelHide.cs
+++ Imagine-game/Assets/scripts/TurtelHide.cs
@@ -4,22 +4,31 @@ public class TurtelHide : MonoBehaviour
 {
     public GameObject turtelActive;
     public GameObject turtelRock;
+
+    private void Start()
+    {
+        turtelActive.SetActive(false);
+        turtelRock.SetActive(true);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            turtelActive.SetActive(false);
-            turtelRock.SetActive(true);
+            Debug.Log("Activate turt");
+            turtelActive.SetActive(true);
+            turtelRock.SetActive(false);
         }
         
+
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            turtelActive.SetActive(true);
-            turtelRock.SetActive(false);
+            Debug.Log("Disable turt");
+            turtelActive.SetActive(false);
+            turtelRock.SetActive(true);
         }
 
     }
